e6eab826c0fe6c7d218dace33eebf2e490c49908
AaronGitH <agor@itu.dk>
2015-08-26 10:05:29 +0200

    parse class

---
+++ JavaApplication4/src/dev/ParseData.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dev;
+
+/**
+ *
+ * @author Group D guys and girls
+ */
+public class ParseData {
+    
+    public void parseFile(String filename){
+        // .....
+        
+    }
+    
+    public int getN(){
+        int n= 4;
+        return n;
+    }
+    
+    public String[] getManNames(){
+        String[] manNames  = {"Sheldon", "Rajesh", "Howard", "Leonard"};
+        return manNames;
+    }
+    public String[] getWomanNames(){
+        String[] womanNames = {"Amy", "Penny", "Bernadette", "Emily"};
+        return womanNames;
+    }
+    
+    public int[][] getWomanPrefs(){
+        int[][] womanPrefs = {{1, 7, 5, 3}, {7, 3, 1, 5}, {5, 3, 7, 1}, {7, 5, 1, 3}};
+        return womanPrefs;
+    }
+    
+    public int[][] getManPrefs(){
+        int[][] manPrefs = {{2, 4, 6, 8},{6, 4, 2, 8},{6, 4, 8, 2},{4, 8, 6, 2}};
+        return manPrefs;
+    }
+    
+        public int[][] getInversWomanPrefs(){
+        int[][] womanPrefs = {}; 
+        return womanPrefs;
+    }
+    
+    public int[][] getInversManPrefs(){
+        int[][] manPrefs = {};
+        return manPrefs;
+    }
+}
