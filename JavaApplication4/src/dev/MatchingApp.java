f65767628b908f1f3935b18813a4194f08945015
AaronGitH <agor@itu.dk>
2015-08-26 11:37:10 +0200

    Algorithm second half, maybe a lot of debugging is still needed

--- JavaApplication4/src/dev/MatchingApp.java
+++ JavaApplication4/src/dev/MatchingApp.java
@@ -1,17 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dev;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
 /**
  *
- * @author user
+ * @author Group D guys and girls
  */
 public class MatchingApp {
 
@@ -23,7 +17,7 @@ public class MatchingApp {
         ParseData parseData = new ParseData();
         parseData.parseFile("");
         
-        int n = 4;
+        int n = parseData.getN();
         String[] manNames  = parseData.getManNames();
         String[] womanNames = parseData.getWomanNames();        
         
@@ -39,30 +33,44 @@ public class MatchingApp {
         int[][] womanPrefs = parseData.getWomanPrefs();
         int[][] manPrefs = parseData.getManPrefs();
         
+        int[][] invertWomanPrefs = new int[n][n];
+        int[][] invertManPrefs = new int[n][n];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                invertWomanPrefs[i][womanPrefs[i][j]] = j;
+                invertManPrefs[i][manPrefs[i][j]] = j;
+            }
+        }
         
         //while(some man m is unmatched and hasn't proposed to every woman)
         while(freeMan.size() != 0){
-            //w← first woman on m's list to whom m has not yet proposed.
-            int currMen = freeMan.poll();
-            
+            int currMan = freeMan.poll();
             for(int i = 0; i < n; i++){
+                //w ← first woman on m's list to whom m has not yet proposed.
+                int w = manPrefs[currMan][i];
                 //IF (w is unmatched)
-                if( wife [ manPrefs[currMen][i] ] == null  ){
+                if( wife[w] == null  ){
                     //Add pair m–w to matching S.
-                   wife[manPrefs[currMen][i]] = currMen;
-                   husband[currMen] = manPrefs[currMen][i];
+                   wife[w] = currMan;
+                   husband[currMan] = w;
                    break; 
                 }
+                //ELSE IF (w prefers m to her current partner m')
+                else if(invertWomanPrefs[w][currMan] < invertWomanPrefs[w][wife[w]]){
+                    //Remove pair m'–w from matching S.
+                    wife[w] = null;
+                    husband[currMan] = null;
+                    //Add pair m–w to matching S.
+                    wife[w] = currMan;
+                    husband[currMan] = w;
+                }
+                //ELSE
+                else{
+                    //w rejects m.
+                }
             }
-            //ELSE IF (w prefers m to her current partner m')
-            
-                //Remove pair m'–w from matching S.
-                //Add pair m–w to matching S.
-            //ELSE
-                //w rejects m.
             //RETURN stable matching S.
         }
-    
     }
     
 }
